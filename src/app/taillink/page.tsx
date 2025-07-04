367f44fee477b83a8fbc54b59390010be1eb0cb1
Syed Maroof <maroof@example.com>
2025-07-03 21:03:28 -0400

    header bar w/ desc completed

---
+++ src/app/taillink/page.tsx
@@ -0,0 +1,19 @@
+import Header from '../Header';
+
+export default function Home() {
+
+  return (
+
+    <>
+
+      <div>
+
+        <Header title = "TailLink" sub_title = "Your semester, built seamlessly." />
+      
+      </div>
+
+    </>
+
+  );
+
+}
