f8da96b12b176c02f979711547a43fe624c2a473
Syed Maroof <syedmarhuss@gmail.com>
2025-07-17 21:51:37 -0400

    opted out for my custom svgs except logo; using some dummy ones right now from lucide-react may or may not change; the icons should be telling of the feature

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -3,60 +3,74 @@
 import React from 'react';
 import { useState } from 'react';
 import Logo from './Logo';
-import TaillinkIcon from './TaillinkSVG';
-import Sidebutton from './Sidebutton';
+import SidebarItem from './SidebarItem';
+import { MoreVertical } from 'lucide-react';
+
+
 
 type sidebar_props = {
 
-  sidebar_items: React.ReactNode;
-  sidebar_user_icon: React.ReactNode; // for now: this will later accept a gmail icon from the user
-  sidebar_user_name: string;
-  sidebar_user_email: string;
+  sidebar_user_icon: React.ReactNode; // to do: this will later accept a gmail icon from the user
+  sidebar_user_name: string; // to do: name of the user's gmail account
+  sidebar_user_email: string; // to do: user's gmail
+  children: React.ReactNode; 
 
 }
 
-export default function Sidebar({sidebar_items, sidebar_user_icon, sidebar_user_name, sidebar_user_email}: sidebar_props) {
+export default function Sidebar({sidebar_user_icon, sidebar_user_name, sidebar_user_email, children}: sidebar_props) {
   
   return (
 
     <>
 
-      <aside className = "h-screen">
+      <aside className = "h-screen"> {/* give it full viewport height */}
 
-        <nav className = "h-full flex flex-col bg-night border-r border-r-neutral-800 shadow-sm">
+        {/* nav bar div; make it flex so the item list can take up the rest of the space*/}
+        <nav className = "h-full flex flex-col bg-night border-r border-r-neutral-800 shadow-sm"> 
 
-          <div className = "pb-2 h-16 flex justify-between items-center gap-1"> 
+          {/* make the logo the button to toggle side bar */}
+          <div className = " h-16 flex justify-between items-center"> 
 
-            <button className = "p-1.5">
+            <button className = "p-1.5 cursor-pointer">
 
-              <Logo className = "w-12 p-1.5 hover:bg-neutral-600 rounded-lg"/>
+              <Logo className = "w-12 p-1.5 hover:bg-neutral-800 rounded-lg transition ease duration-200"/>
               
             </button>
 
+
           </div>
 
-          <ul className = "flex-1 px-3"> 
+          <ul className = "flex-1 px-3"> {/* flex-1 makes this take up the rest of the space of the div*/}
 
-            { sidebar_items }
-            
+            { children } {/* accept passed in children as sidebar items */}
+
           </ul>
 
           <div className = "border-t border-t-neutral-800 flex p-3">
 
-            <div className="w-10 h-10 rounded-md overflow-hidden bg-neutral-700 flex items-center justify-center">
+            {/* div for the gmail user icon */}
+            <div className="w-10 h-10 rounded-md overflow-hidden bg-neutral-800 flex items-center justify-center">
             
               {sidebar_user_icon}
 
             </div>
 
+            {/* div for the gmail name/email text */}
             <div className = "flex justify-between items-center w-40 ml-3">
 
               <div className = "leading-4">
 
-                <h4 className = "font-semibold text-asparagus">{sidebar_user_name}</h4>
-                <span className = "text-xs font-semibold text-lime-900">{sidebar_user_email}</span>
+                <h4 className = "text-asparagus">{sidebar_user_name}</h4>
+                <span className = "text-xs text-lime-900">{sidebar_user_email}</span>
 
               </div>
+
+              {/* three dot icon taken from lucide react */}
+              <button className='cursor-pointer'>
+                
+                <MoreVertical color='#698f3f' size={20} className="hover:bg-neutral-800 rounded-lg transition ease duration-200"/>
+                
+              </button>
 
             </div>
 
