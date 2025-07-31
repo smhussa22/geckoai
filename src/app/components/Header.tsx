e95515431785fefaaa76b14eb1d8c7072f44cee4
Syed Maroof <syedmarhuss@gmail.com>
2025-07-31 02:08:00 -0400

    implemented hook to get user data, switched user menu to be in header, fixed header metadata to respond, implemented prisma into project

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,33 +2,36 @@
 'use client';
 import React from 'react';
 import { useState } from 'react';
-
-// todo: need to import the logo as a component later to animate it 
-// import geckoLogo from '/logo.svg'; 
-
-type header_descriptions = {
-
-  title: string;
-  sub_title: string;
-
-}
-
-export default function Header({title, sub_title}: header_descriptions) {
+import { usePathname } from 'next/navigation';
+import { routeMetadata } from '../routeMetadata';
+import { Tooltip } from 'react-tooltip';
+export default function Header() {
 
   const [isToggled, toggle] = useState(false);
+  const path_name = usePathname();
+
+  const metadata_key = path_name.split("/").filter(Boolean)[0];
+
+  const metadata = routeMetadata[metadata_key];
 
   return (
 
     <>
 
-      <div className = "w-full h-16 items-center flex border-b border-b-neutral-800">
+      <div className = "w-full items-center flex border-b border-b-neutral-800 px-4 py-3">
 
-        <div className = "px-3">
+        <div>
 
-          <h1 className = "text-asparagus text-2xl font-semibold">{`${title}`}</h1>
-          <p className = "text-broccoli ml-0.5">{`${sub_title}`}</p>
+          <h1 className = "text-asparagus text-2xl font-semibold">{metadata.title}</h1>
+          <p className = "text-broccoli ml-0.5">{metadata.sub_title}</p>
           
         </div>
+
+        <button data-tooltip-id = "gmail_icon" data-tooltip-content = "test" className = 'data-tooltip-target bg-neutral-800 w-12 aspect-square rounded-full ml-auto'>
+
+        </button>
+
+        <Tooltip id="gmail_icon" place="bottom" opacity={1} style={{ marginLeft: '0.5rem', backgroundColor: '#262626', padding: '0.4rem', borderRadius: '0.375rem', transitionProperty: 'color', transitionDuration: '300ms'}} noArrow delayShow={0} delayHide={0}/>
 
       </div>
 
