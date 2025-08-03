475305f21c2efc2f1549b1d39d879c540d38d235
Syed Maroof <syedmarhuss@gmail.com>
2025-08-03 19:11:49 -0400

    used a context to get the info so i removed authfunctions

--- src/app/contexts/UserContext.tsx
+++ src/app/contexts/UserContext.tsx
@@ -1,25 +1,27 @@
 'use client';
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 
 interface User {
 
-    firstName: string;
-    lastName: string;
+    name: string;
+    email: string;
     picture: string;
 
 }
 
-const UserContext = createContext<{
+interface UserContextType {
 
-    user: User | null;
-    setUser: (user: User | null) => void;
-    handleGoogleLogOut: () => void;
+    user: User | null,
+    GoogleLogIn: () => void;
+    GoogleLogOut: () => void;
 
-}>({
+}
+
+const UserContext = createContext<UserContextType>({
 
     user: null,
-    setUser: (user: User | null) => {},
-    handleGoogleLogOut: () => {},
+    GoogleLogIn: () => {},
+    GoogleLogOut: () => {}
 
 });
 
@@ -27,18 +29,79 @@ export const UserProvider = ({children}: {children: React.ReactNode}) => {
 
     const [user, setUser] = useState<User | null>(null);
 
-    const handleGoogleLogOut = () => {
+    useEffect(() => {
+
+        const token = localStorage.getItem('googleJWT');
+        if (!token) return;
+
+        try{
+
+            const userObject = JSON.parse(atob(token.split('.')[1]));
+            
+            setUser({
+
+                picture: userObject.picture,
+                name: userObject.name,
+                email: userObject.email
+
+            });
+
+        }
+        catch(error: any){
+
+            console.error(error);
+
+        }
+
+    }, []);
+
+    const GoogleLogIn = () => {
+
+        const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+
+        if(!clientId) return console.error("Google Client ID is inaccessible.");
+
+        (window as any).google.accounts.id.initialize({
+
+            client_id: clientId,
+            callback: (response: any) => {
+
+                if (!response || !response.credential) return null;
+
+                localStorage.setItem('googleJWT', response.credential);
+
+                const userObject = JSON.parse(atob(response.credential.split('.')[1]));
+
+                const userInfo = {
+
+                    picture: userObject.picture,
+                    name: userObject.name,
+                    email: userObject.email
+
+                };
+
+                setUser(userInfo);
+
+            }      
+
+        });
+
+        (window as any).google.accounts.id.prompt();
+
+    }
+
+    const GoogleLogOut = () => {
 
         localStorage.removeItem('googleJWT');
         setUser(null);
 
-    };
-    
-    return(
+    }
 
-        <UserContext.Provider value = { {user, setUser, handleGoogleLogOut }}>
+    return (
 
-            {children};
+        <UserContext.Provider value = {{ user, GoogleLogIn, GoogleLogOut}}>
+
+            {children}
 
         </UserContext.Provider>
 
