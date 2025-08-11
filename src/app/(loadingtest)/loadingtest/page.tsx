7959feddddab9d1c4abf77f54faaf14f7a1956fe
cssyed <syedmarhuss@gmail.com>
2025-08-11 02:58:24 -0400

    push

---
+++ src/app/(loadingtest)/loadingtest/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react';
+import Loading from '../../loading';
+import Logo
+
+export default function HomePage() {
+
+
+  return (
+
+    <>
+
+        <Loading/>
+    
+    </>
+
+  );
+  
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { UserProvider } from "./contexts/UserContext";
 import Script from "next/script";
+import './globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
