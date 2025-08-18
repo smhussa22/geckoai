2cc10840eb9bcfce490a64feed17bf32852e76ae
cssyed <syedmarhuss@gmail.com>
2025-08-17 23:44:38 -0400

    pushing

--- src/app/(error)/layout.tsx
+++ src/app/(error)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({children}: { children: React.ReactNode; }) {
   return (
 
       
-    <div className="auth-layout flex items-center justify-center h-screen bg-night">
+    <div className="auth-layout flex flex-row r h-screen bg-night">
     
       {children}
       
--- src/app/(misc)/layout.tsx
+++ src/app/(misc)/layout.tsx
@@ -4,7 +4,8 @@ export default function AuthLayout({children}: { children: React.ReactNode; }) {
 
   return (
 
-      <div className="auth-layout flex items-center justify-center h-screen bg-night">
+    <div className="auth-layout flex flex-col p-4 h-screen bg-night">
+      
       {children}
       
     </div>
